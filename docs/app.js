9f8a0190529bfb2f9f6f50397c3b83875dcdd8c3
WanderingByteSage <WanderingByteSage@gmail.com>
2024-10-05 14:06:43 -0400

    weekly api update

--- docs/app.js
+++ docs/app.js
@@ -211,9 +211,9 @@ async function filterGames() {
   // (3) Goes here...
 
   // Fetch data from the ESPN API
-   const apiUrl = 'https://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard?dates=20240926-20240929';
+   const apiUrl = 'https://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard?dates=20241005-20241005';
   // const apiUrl = "https://wanderingbytesage.github.io/sample.json"; // Adjust the path if necessary
-  //  const apiUrl = "https://wanderingbytesage.github.io/scoreboard.json"; // Adjust the path as necessary
+  //const apiUrl = "https://wanderingbytesage.github.io/scoreboard.json"; // Adjust the path as necessary
 
   try {
     const response = await fetch(apiUrl);
